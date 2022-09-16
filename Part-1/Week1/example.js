dd3bd39b44d5f18cf81a30ae75fe8cf574575582
Juliet Kim <jkim916@my.bcit.ca>
2022-09-16 12:15:25 -0700

    adding more code

--- Part-1/Week1/example.js
+++ Part-1/Week1/example.js
@@ -1,4 +1,27 @@
-// Declare a variable called "car" 
+//Boolean 
+var proceed = true;
+var i = 0;
 
-var car; 
+while(proceed) {
+i++; i = 1;
+proceed = false;
 
+};
+console.log(i);
+
+
+//Data Type - object
+/**
+ * 6-4-1 create a variable called grannySmith and add the following values (camelCase)
+ * colour of apple is green 
+ * origin location is in Australia
+ * origin date is 1868
+ * the concole log out the colour from the object
+ */
+var grannySmith = {
+    colour: "green",
+    originLocation: "Australia",
+    originDate: 1868
+};
+
+console.log (grannySmith);
